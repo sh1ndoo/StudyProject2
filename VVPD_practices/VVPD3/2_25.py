b270352f211c93cfa689a00a0ebf5501cf086a6e
shindoo <trofim-dima06@mail.ru>
2024-10-16 12:06:27 +0700

    Task 2 complete

---
+++ VVPD_practices/VVPD3/2_25.py
@@ -0,0 +1,12 @@
+def find_multiple_in_a_multiple_in_b(A, B):
+    multiple_in_A = {x for x in A if A.count(x) > 1}
+    multiple_in_B = {x for x in B if B.count(x) > 1}
+    result = [x for x in multiple_in_A if x in multiple_in_B]
+    return result
+
+
+# Пример массивов A и B
+A = [1, 2, 3, 4, 5, 6, 2, 5, 7]
+B = [2, 2, 3, 3, 7, 8, 9, 3]
+
+print(find_multiple_in_a_multiple_in_b(A, B))
